7057be7c9c5d00a54b0193686f85e1a64479b119
Shivansh Pandey <99907935+shivpandey02@users.noreply.github.com>
2022-03-02 20:31:02 +0530

    Add files via upload

---
+++ Pandey/question_2.java
@@ -0,0 +1,10 @@
+// addition of two numbers
+class question_2 {
+    public static void main(String[] args) {
+        int a=6;
+        int b=6;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+    
+}
