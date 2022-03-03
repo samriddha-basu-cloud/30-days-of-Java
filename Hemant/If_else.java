4dad746ff782e5084a4062c618c2cad9a182cbaa
Hemantgarg06 <99550853+Hemantgarg06@users.noreply.github.com>
2022-03-03 15:37:31 +0530

    Add files via upload

---
+++ Hemant/If_else.java
@@ -0,0 +1,12 @@
+public class If_else {
+    public static void main(String[] args) {
+    int a = 21;
+if (a>18) {
+        System.out.println("You can drive");
+    }
+else{
+        System.out.println("You can't drive");
+    }
+
+}
+}
