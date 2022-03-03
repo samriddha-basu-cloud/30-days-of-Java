fd51644dd9784a5f7eb7a47d04da240fa80650dc
Anubhav140 <77580549+Anubhav140@users.noreply.github.com>
2022-03-03 11:53:44 -0500

    Add files via upload

---
+++ Anubhav/table.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int num = 7;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
