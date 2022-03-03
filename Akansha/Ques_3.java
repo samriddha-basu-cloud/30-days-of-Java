41e8da4d840c0e2ad7051c86a6ab5b48b12cf4f2
AKANSHA SHREE <81106541+akansha961@users.noreply.github.com>
2022-03-03 22:40:05 +0530

    Add files via upload

---
+++ Akansha/Ques_3.java
@@ -0,0 +1,18 @@
+class Third {
+    public static void main(String[] args) 
+{ 
+ int x = 200; 
+ int y = 50; 
+ int z = 100; 
+if(x > y && y > z)
+{ 
+  System.out.println("Hi"); 
+} 
+if(z > y && z < x)
+{ 
+  System.out.println("Hello"); 
+} 
+} 
+}
+    
+
