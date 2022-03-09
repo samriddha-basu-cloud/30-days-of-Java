da489ab4464549333528e6ee5c5ba4693a7a2350
AKANSHA SHREE <81106541+akansha961@users.noreply.github.com>
2022-03-09 19:19:43 +0530

    Add files via upload

---
+++ Akansha/Ques_9.java
@@ -0,0 +1,10 @@
+class ninth {
+    public static void main(String args[]){
+         int arr[]={3,8,16,25};
+         int i=0;
+         do{
+              System.out.println(arr[i]);
+              i++;
+         }while(i<4);
+    }
+}
